8a3098805b9745f8a40884d5ec9f9b01a10c8b40
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-11-27 17:39:58 +0000

    Updated app..js
    Added small changed to index.html

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 const squares = document.querySelectorAll('.grid div');
+const result = document.querySelector('#result');
+const displayCurrentPlayer = document.querySelector('#current-player');
+
+let currentPlayer = 1;
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+        alert('You have click square' + i);
+    }
+}
+
+
+result.innerHTML = 'Hello';
+})
+
+
